4bacc4bb170ba4ce07ef4a9bfcbb7475e5a8b30d
micelife <644863737@qq.com>
2016-06-08 16:52:15 +0800

    add getClass Result to cache

--- mic4j-util/src/main/java/org/mic4j/framework/util/base/ClassHelper.java
+++ mic4j-util/src/main/java/org/mic4j/framework/util/base/ClassHelper.java
@@ -1,19 +1,24 @@
 package org.mic4j.framework.util.base;
 
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ClassHelper {
 	private final static String BASE_PACKAGE_FRAMEWORK = "org.mic4j.framework";
-	private final static Map<String, List<Class<?>>> classMapCache = new ConcurrentHashMap<String, List<Class<?>>>();
+	private final static ConcurrentHashMap<String, List<Class<?>>> classMapCache = new ConcurrentHashMap<String, List<Class<?>>>();
 
 	static {
 		getClass(BASE_PACKAGE_FRAMEWORK);
 	}
 
 	public static List<Class<?>> getClass(String pckPath) {
-		return null;
+		if (classMapCache.containsKey(pckPath))
+			return classMapCache.get(pckPath);
+
+		List<Class<?>> theList = null;
+		classMapCache.put(pckPath, theList);
+
+		return theList;
 	}
 
 	public static List<Class<?>> getSubClass(String pckPath, Class<?> superClass) {
